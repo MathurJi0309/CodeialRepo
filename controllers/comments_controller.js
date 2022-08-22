fb7f117083f469c6deba5d2faf12457c0be6f500
MathurJi <anileshjjssacademy84@gmail.com>
2022-08-22 21:47:25 +0530

    now add mailers

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,30 +1,46 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
+const commentsMailer = require('../mailers/comments_mailer');
+module.exports.create = async function(req, res){
 
-module.exports.create = function(req, res){
-    Post.findById(req.body.post, function(err, post){
+    try{
+        let post = await Post.findById(req.body.post);
 
         if (post){
-            Comment.create({
+            let comment = await Comment.create({
                 content: req.body.content,
                 post: req.body.post,
                 user: req.user._id
-            }, function(err, comment){
-                // handle error
-                if(err){
-                    console.log(err,'error in push comment');
-                    return;
-                }
-                console.log(post.comments);
-                post.comments.push(comment);
-                post.save();
-
-                return res.redirect('/');
             });
-        }
 
-    });
+            post.comments.push(comment);
+            post.save();
+            
+            comment = await comment.populate('user', 'name email').execPopulate();
+            commentsMailer.newComment(comment);
+            if (req.xhr){
+                
+    
+                return res.status(200).json({
+                    data: {
+                        comment: comment
+                    },
+                    message: "Post created!"
+                });
+            }
+
+
+            req.flash('success', 'Comment published!');
+
+            res.redirect('/');
+        }
+    }catch(err){
+        req.flash('error', err);
+        return;
+    }
+    
 }
+
 
 module.exports.destroy=function(req,res){
     Comment.findById(req.params.id,function(err,comment){
